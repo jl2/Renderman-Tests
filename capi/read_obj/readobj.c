4ced48045a3416aa2a79dd0d0a431bfb01b38cb5
Jeremiah LaRocco <jeremiah.larocco@gmail.com>
2012-02-29 23:37:44 -0700

    Read faces, normals and texture coordinates.  Need to refactor a bit.

--- capi/read_obj/readobj.c
+++ capi/read_obj/readobj.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 enum obj_entry_type {vertex, normal, text_coord, face, object, comment, bad};
 
@@ -32,11 +33,50 @@ typedef struct wave_object_s {
     size_t num_verts;
     size_t num_texts;
     size_t num_norms;
+    size_t num_faces;
     vertex_t *verts;
     text_coord_t *text_coords;
     normal_t *norms;
     face_t *faces;
+    char *name;
 } wave_object_t;
+
+void init_object(wave_object_t *obj) {
+    obj->num_verts = 0;
+    obj->num_norms = 0;
+    obj->num_texts = 0;
+    obj->num_faces = 0;
+    
+    obj->verts = NULL;
+    obj->text_coords = NULL;
+    obj->norms = NULL;
+    obj->faces = NULL;
+    obj->name = NULL;
+}
+
+void free_object(wave_object_t *obj) {
+    if (obj->num_verts>0  &&  obj->verts!=NULL) {
+        free(obj->verts);
+        obj->verts = NULL;
+        obj->num_verts = 0;
+    }
+    if (obj->num_norms>0  &&  obj->norms!=NULL) {
+        free(obj->norms);
+        obj->norms = NULL;
+        obj->num_norms = 0;
+    }
+    if (obj->num_texts>0  &&  obj->text_coords!=NULL) {
+        free(obj->text_coords);
+        obj->text_coords = NULL;
+        obj->num_texts = 0;
+    }
+    if (obj->num_faces>0  &&  obj->faces!= NULL) {
+        free(obj->faces);
+        obj->faces = NULL;
+        obj->num_texts = 0;
+    }
+    
+}
 
 int is_obj_comment(char *ins) {
     size_t i=0;
@@ -64,6 +104,27 @@ const char *type_to_string(enum obj_entry_type ot) {
     /*     return "unknown type"; */
     }
 }
+
+int read_object_type(char *txt, wave_object_t *obj) {
+    
+    size_t i = 0;
+
+    // Verify it's a 'o'
+    // Find the 'o'
+    while (isspace(txt[i])) ++i;
+    if (txt[i] != 'o') {
+        return 0;
+    }
+    // Skip it
+    ++i;
+    while (isspace(txt[i])) ++i;
+
+    size_t nlen = strlen(txt+i);
+    obj->name = malloc(nlen+1);
+    strcpy(obj->name, txt+i);
+    return 1;
+}
+
 enum obj_entry_type get_entry_type(char *buff) {
     char *space_pos = NULL;
     enum obj_entry_type retVal = bad;
@@ -103,7 +164,9 @@ enum obj_entry_type get_entry_type(char *buff) {
     return retVal;
 }
 
-void preprocess(FILE* inf, size_t *num_verts, size_t *num_norms, size_t *num_texts, size_t *num_faces, size_t *num_objs) {
+
+
+void preprocess(FILE* inf, wave_object_t *obj) {
     /*
       Scan through the file and count the number of vertices, texture
       coordinates, normals and faces
@@ -149,6 +212,7 @@ void preprocess(FILE* inf, size_t *num_verts, size_t *num_norms, size_t *num_tex
             break;
         case object:
             ++no;
+            read_object_type(in_buffer, obj);
             break;
         default:
             break;
@@ -156,23 +220,213 @@ void preprocess(FILE* inf, size_t *num_verts, size_t *num_norms, size_t *num_tex
             
         /* printf("\"%s\" is a %s.\n", in_buffer, type_to_string(get_entry_type(in_buffer))); */
     }
-    *num_verts = nv;
-    *num_norms = nn;
-    *num_texts = nt;
-    *num_faces = nf;
-    *num_objs = no;
+    obj->num_verts = nv;
+    obj->verts = malloc(sizeof(vertex_t) * nv);
+    
+    obj->num_norms = nn;
+    obj->norms = malloc(sizeof(normal_t) * nn);
+    
+    obj->num_texts = nt;
+    obj->text_coords = malloc(sizeof(text_coord_t) * nt);
+    
+    obj->num_faces = nf;
+    obj->faces = malloc(sizeof(face_t) * nf);
 
     // Return to original position
     fsetpos(inf, &original_pos);
 }
+
+void read_data(FILE *inf, wave_object_t *obj) {
+    // Go to the beginnning
+    rewind(inf);
+
+    double xt, yt, zt;
+    double it, jt, kt;
+    double st, tt;
+    size_t num_pts;
+
+    size_t i;
+    size_t j;
+    size_t type_pos;
+    size_t next_space;
+    char tmp;
+    size_t cur_vert = 0;
+    size_t cur_norm = 0;
+    size_t cur_text = 0;
+    size_t cur_face = 0;
+    char in_buffer[512] = "";
+    size_t blen = 0;
+
+        
+    while (!feof(inf)) {
+        fgets(in_buffer, 512, inf);
+        blen = strlen(in_buffer);
+        in_buffer[blen-1] = '\0';
+        --blen;
+        if (in_buffer[blen-1] == '\r') {
+            in_buffer[blen-1] ='\0';
+            --blen;
+        }
+        enum obj_entry_type obj_type = get_entry_type(in_buffer);
+
+        switch (obj_type) {
+        case vertex:
+            // Find the 'v' and skip it and the white space after it
+            i = strchr(in_buffer, 'v')  - in_buffer + 1;
+            while (isspace(in_buffer[i])) ++i;
+            j = i;
+            while (!isspace(in_buffer[j])) ++j;
+            tmp = in_buffer[j];
+            in_buffer[j] = '\0';
+            xt = atof(in_buffer+i);
+            in_buffer[j] = tmp;
+
+            i = j;
+            while (isspace(in_buffer[i])) ++i;
+            j = i;
+            while (!isspace(in_buffer[j])) ++j;
+            tmp = in_buffer[j];
+            in_buffer[j] = '\0';
+            yt = atof(in_buffer+i);
+            in_buffer[j] = tmp;
+
+            i = j;
+            while (isspace(in_buffer[i])) ++i;
+            j = i;
+            while (!isspace(in_buffer[j])) ++j;
+            tmp = in_buffer[j];
+            in_buffer[j] = '\0';
+            zt = atof(in_buffer+i);
+            in_buffer[j] = tmp;
+            printf("Read vertex %zu, which is: %f %f %f\n", cur_vert,xt,yt,zt);
+
+            obj->verts[cur_vert].x = xt;
+            obj->verts[cur_vert].y = yt;
+            obj->verts[cur_vert].z = zt;
+            
+            ++cur_vert;
+            break;
+
+        case normal:
+            // Find the 'n' and skip it and the white space after it
+            i = strchr(in_buffer, 'n')  - in_buffer + 1;
+            while (isspace(in_buffer[i])) ++i;
+            j = i;
+            while (!isspace(in_buffer[j])) ++j;
+            tmp = in_buffer[j];
+            in_buffer[j] = '\0';
+            it = atof(in_buffer+i);
+            in_buffer[j] = tmp;
+
+            i = j;
+            while (isspace(in_buffer[i])) ++i;
+            j = i;
+            while (!isspace(in_buffer[j])) ++j;
+            tmp = in_buffer[j];
+            in_buffer[j] = '\0';
+            jt = atof(in_buffer+i);
+            in_buffer[j] = tmp;
+
+            i = j;
+            while (isspace(in_buffer[i])) ++i;
+            j = i;
+            while (!isspace(in_buffer[j])) ++j;
+            tmp = in_buffer[j];
+            in_buffer[j] = '\0';
+            kt = atof(in_buffer+i);
+            in_buffer[j] = tmp;
+            printf("Read normal %zu, which is: %f %f %f\n", cur_norm,it,jt,kt);
+
+            obj->norms[cur_norm].i = it;
+            obj->norms[cur_norm].j = jt;
+            obj->norms[cur_norm].k = kt;
+
+            ++cur_norm;
+            
+            break;
+
+        case text_coord:
+            // Find the "vt" and skip it and the white space after it
+            i = strchr(in_buffer, 'v')  - in_buffer + 2;
+            while (isspace(in_buffer[i])) ++i;
+            j = i;
+            while (!isspace(in_buffer[j])) ++j;
+            tmp = in_buffer[j];
+            in_buffer[j] = '\0';
+            st = atof(in_buffer+i);
+            in_buffer[j] = tmp;
+
+            i = j;
+            while (isspace(in_buffer[i])) ++i;
+            j = i;
+            while (!isspace(in_buffer[j])) ++j;
+            tmp = in_buffer[j];
+            in_buffer[j] = '\0';
+            tt = atof(in_buffer+i);
+            in_buffer[j] = tmp;
+
+            printf("Read texture coordinate %zu, which is: %f %f\n", cur_text,st,tt);
+
+            obj->text_coords[cur_text].s = st;
+            obj->text_coords[cur_text].t = tt;
+
+            ++cur_text;
+
+            break;
+
+        case face:
+            // Find the 'f' and skip it and the white space after it
+            i = strchr(in_buffer, 'f')  - in_buffer + 1;
+            while (isspace(in_buffer[i])) ++i;
+            
+            j = i;
+            while (!isspace(in_buffer[j])) ++j;
+            tmp = in_buffer[j];
+            in_buffer[j] = '\0';
+            it = atof(in_buffer+i);
+            in_buffer[j] = tmp;
+
+            i = j;
+            while (isspace(in_buffer[i])) ++i;
+            j = i;
+            while (!isspace(in_buffer[j])) ++j;
+            tmp = in_buffer[j];
+            in_buffer[j] = '\0';
+            jt = atof(in_buffer+i);
+            in_buffer[j] = tmp;
+
+            i = j;
+            while (isspace(in_buffer[i])) ++i;
+            j = i;
+            while (!isspace(in_buffer[j])) ++j;
+            tmp = in_buffer[j];
+            in_buffer[j] = '\0';
+            kt = atof(in_buffer+i);
+            in_buffer[j] = tmp;
+            printf("Read normal %zu, which is: %f %f %f\n", cur_norm,it,jt,kt);
+            
+            break;
+
+        case object:
+            break;
+
+        default:
+            break;
+        }
+            
+        /* printf("\"%s\" is a %s.\n", in_buffer, type_to_string(get_entry_type(in_buffer))); */
+    }
+    
+}
+void read_object(FILE *inf, wave_object_t *obj) {
+    preprocess(inf, obj);
+    read_data(inf, obj);
+}
+
 int main(int argc, char *argv[]) {
     FILE *inf;
-    size_t nv=0;
-    size_t nn=0;
-    size_t nt=0;
-    size_t nf=0;
-    size_t no = 0;
-    
+    wave_object_t obj;
+
     if (argc <2) {
         printf("No input file name given!\n");
         return 1;
@@ -182,9 +436,15 @@ int main(int argc, char *argv[]) {
         printf("Could not open \"%s\"\n", argv[1]);
         return 1;
     }
+    init_object(&obj);
+    read_object(inf, &obj);
+    
+    
+    printf("Object file has:\n  %zu vertices\n  %zu normals\n  %zu texture coordinates\n  %zu faces\n  %d objects",
+           obj.num_verts, obj.num_norms, obj.num_texts, obj.num_faces, 1);
 
-    preprocess(inf, &nv, &nn, &nt, &nf, &no);
-    printf("Object file has:\n  %zu vertices\n  %zu normals\n  %zu texture coordinates\n  %zu faces\n  %zu objects", nv,nn,nt,nf, no);
+    free_object(&obj);
+
     fclose(inf);
     return 0;
 }
